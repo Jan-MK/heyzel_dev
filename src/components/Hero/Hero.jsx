edf238ee7c23e98beac4c73489c544abece1c466
Jan Krämer <jan.kraemer@tum.de>
2024-04-08 13:13:02 +0200

    Lazy fetch images by initial thumbnail rendering and fetching afterwards

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -59,7 +59,7 @@ let countPerColumn = 3
 
 
 function HeroComponent() {
-    const { t, i18n } = useTranslation();
+    const {t, i18n} = useTranslation();
     const leftContainer = useRef(null)
     const rightContainer = useRef(null)
     const containerRef = useRef(null)
@@ -105,115 +105,105 @@ function HeroComponent() {
 
         let mm = gsap.matchMedia()
         if (leftColumn && rightColumn && heroContainer) {
-            const tlLeft = gsap.timeline({
-                scrollTrigger: {
-                    trigger: heroContainer,
-                    start: 'center center',
-                    end: "bottom top",
-                    scrub: 1,
-                },
-            });
+            const trigger = {
+                trigger: heroContainer,
+                start: 'center center',
+                end: "bottom top",
+                scrub: 1,
+            }
 
-            const tlRight = gsap.timeline({
-                scrollTrigger: {
-                    trigger: heroContainer,
-                    start: 'center center',
-                    end: "bottom top",
-                    scrub: 1,
-                },
-            });
 
 
             mm.add(`(min-width: ${minWidthTablet}px)`, () => {
-                let tl = gsap.timeline();
-                tl.add(gsap.from(`.${classes.heroImages}`, 0.75, {
+                gsap.from(`.${classes.heroImages}`, .15, {
                     clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
                     scale: 1,
                     ease: "power2.inOut"
-                }))
-/*                tl.add(gsap.from(`.${classes.hero}`, .5, {
+                })
+                gsap.from(`.${classes.hero}`, .15, {
                     opacity: 0,
                     ease: "power2.inOut"
-                }))*/
-                tlLeft.to(leftColumn, {yPercent: -20});
-                tlRight.to(rightColumn, {yPercent: -15});
+                })
+                gsap.to(leftColumn, {yPercent: -20, scrollTrigger: trigger});
+                gsap.to(rightColumn, {yPercent: -15, scrollTrigger: trigger});
             })
             mm.add(`(max-width: ${maxWidthMobile}px)`, () => {
-                gsap.from(`.${classes.textContent}`, .5, {
+                gsap.from(`.${classes.textContent}`, .15, {
                     opacity: 0,
-                    delay: .5,
                     ease: "power2.inOut",
                 })
-                tlLeft.to(leftColumn, {xPercent: 10});
-                tlRight.to(rightColumn, {xPercent: -5});
+                gsap.to(leftColumn, {xPercent: 10, scrollTrigger: trigger});
+                gsap.to(rightColumn, {xPercent: -5, scrollTrigger: trigger});
             })
         }
     })
 
     return (
-            <div className={classes.heroWrapper} id={'hero'} ref={heroWrapper}>
-                <div className={`${classes.heroImages}`}>
-                    <div className={`${classes.imageArray} ${classes.left}`} id={'leftHeroContainer'}
-                         ref={leftContainer}>
-                        {images2.map((image, idx) => {
-                            return <div className={classes.imageContainer} key={idx}>
-                                {/*<img
+        <div className={classes.heroWrapper} id={'hero'} ref={heroWrapper}>
+            <div className={`${classes.heroImages}`}>
+                <div className={`${classes.imageArray} ${classes.left}`} id={'leftHeroContainer'}
+                     ref={leftContainer}>
+                    {images2.map((image, idx) => {
+                        return <div className={classes.imageContainer} key={idx}>
+                            {/*<img
                                     loading="lazy"
                                     className={classes.heroImage}
                                      src={image[0]}
                                      alt={idx}/>*/}
-                                <BlurryLoadingImage
-                                    lazy={false}
-                                    alt={`Heyzel-Coffee-HeroImage${idx}`}
-                                    preview={image[1]}
-                                    image={image[0]}
-                                    objFit={"cover"}
-                                    imageStyleClass={classes.heroImage}
-                                />
-                            </div>
-                        })}
-                    </div>
-                    <div className={`${classes.imageArray} ${classes.right}`} id={'rightHeroContainer'}
-                         ref={rightContainer}>
-                        {rightSlice.map((image, idx) => {
-                            return <div className={classes.imageContainer} key={idx}>
-                                {/*<img loading="lazy"
+                            <BlurryLoadingImage
+                                lazy={false}
+                                alt={`Heyzel-Coffee-HeroImage${idx}`}
+                                preview={image[1]}
+                                image={image[0]}
+                                objFit={"cover"}
+                                imageStyleClass={classes.heroImage}
+                            />
+                        </div>
+                    })}
+                </div>
+                <div className={`${classes.imageArray} ${classes.right}`} id={'rightHeroContainer'}
+                     ref={rightContainer}>
+                    {rightSlice.map((image, idx) => {
+                        return <div className={classes.imageContainer} key={idx}>
+                            {/*<img loading="lazy"
                                      className={classes.heroImage}
                                      src={image[0]}
                                      alt={idx}/>*/}
-                                <BlurryLoadingImage
-                                    lazy={false}
-                                    alt={`Heyzel-Coffee-HeroImage${idx}`}
-                                    preview={image[1]}
-                                    image={image[0]}
-                                    objFit={"cover"}
-                                    imageStyleClass={classes.heroImage}
-                                />
-                            </div>
-                        })}
-                    </div>
-                </div>
-                <div className={`${classes.hero} container`} ref={containerRef}>
-                    <div className={`${classes.textContent}`}>
-                        <div className={classes.headLine}>
-                            <div className={classes.logoWrapper}>
-                                <Logo width={"100%"}/>
-                            </div>
-                            <p>A <span>unique</span> vibe</p>
+                            <BlurryLoadingImage
+                                lazy={false}
+                                alt={`Heyzel-Coffee-HeroImage${idx}`}
+                                preview={image[1]}
+                                image={image[0]}
+                                objFit={"cover"}
+                                imageStyleClass={classes.heroImage}
+                            />
                         </div>
-                        <p>{t('hero.dscr')}</p>
-                        <button className={'third'}
-                                onClick={() => i18n.changeLanguage(i18n.resolvedLanguage === 'en' ? 'de' : 'en' )}>
-                            {t('general.switch')}
-                            <ReactCountryFlag svg style={{width: '25px', height: 'auto'}} aria-label={i18n.resolvedLanguage === 'en' ? 'Language German' : 'Language English'} countryCode={i18n.resolvedLanguage === 'en' ? 'DE' : 'GB' }/>
-                        </button>
-                    </div>
+                    })}
                 </div>
-                <div className={classes.scrollIconContainer} onClick={handleScrollDownClick}>
-                    <div className={classes.scroll}></div>
-                </div>
-                {isSmartphone && <div className={classes.overlay}></div>}
             </div>
+            <div className={`${classes.hero} container`} ref={containerRef}>
+                <div className={`${classes.textContent}`}>
+                    <div className={classes.headLine}>
+                        <div className={classes.logoWrapper}>
+                            <Logo width={"100%"}/>
+                        </div>
+                        <p>A <span>unique</span> vibe</p>
+                    </div>
+                    <p>{t('hero.dscr')}</p>
+                    <button className={'third'}
+                            onClick={() => i18n.changeLanguage(i18n.resolvedLanguage === 'en' ? 'de' : 'en')}>
+                        {t('general.switch')}
+                        <ReactCountryFlag svg style={{width: '25px', height: 'auto'}}
+                                          aria-label={i18n.resolvedLanguage === 'en' ? 'Language German' : 'Language English'}
+                                          countryCode={i18n.resolvedLanguage === 'en' ? 'DE' : 'GB'}/>
+                    </button>
+                </div>
+            </div>
+            <div className={classes.scrollIconContainer} onClick={handleScrollDownClick}>
+                <div className={classes.scroll}></div>
+            </div>
+            {isSmartphone && <div className={classes.overlay}></div>}
+        </div>
     )
 }
 
